39582ed72e036696f44af78e36508db7c360f715
KhadijaVitaeva <jamalvit9595@gmail.com>
2025-02-21 23:24:39 +0300

    2

--- src/funcs.py
+++ src/funcs.py
@@ -2,14 +2,12 @@ import json
 from category_class import Category
 from product_class import Product
 
-
 def load_data() -> list:
     """ Загружает список категорий и товаров из файла """
 
     with open('products.json', encoding='utf-8') as file:
         data = json.load(file)
         return data
-
 
 def create_class_objects(data: list) -> list[Category]:
     """ Создает объекты класса категория и класса продукт """
